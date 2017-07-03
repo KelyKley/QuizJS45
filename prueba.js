96e05c77e7c6578e7c0cbbeda3cc97580e99d4cb
Kely <kelyanamurohuamani@gmail.com>
2017-07-03 15:22:42 -0500

    Ejercicio

--- prueba.js
+++ prueba.js
@@ -5,12 +5,21 @@ var mini = (Math.min(10,30,5));
 console.log("Máximo = "+ maxi + "   Mínimo = " +mini );
 
 //PROBLEMA 2 : Dado un arreglo A de números, imprimir los elementos con índice par e impar por separado. Hint : usar el operador​	 ​ módulo(%).​
-var arr = [15,  29, 78, 29, 40, 20, 18, 100, 78, 68]
+var par = [];
+var impar=[];
+var arr = [15,  29, 78, 29, 40, 20, 18, 100, 78, 68] ;
 
-for (var i = 0; i<i.length; i++){
-  if(arr[i]%2==0)
-    console.log(arr);
+function mostrar(arr){
+for (var i=0; i<arr.length;i++){
+  if(i%2==0)
+    par.push(arr[i]);
+  else
+    impar.push(arr[i]);
 }
+  console.log ("Elementos de posición par : "+ par);
+  console.log("Elementos de posición impar : "+ impar);
+}
+mostrar(arr);
 
 //PROBLEMA 3 :  Dado un arreglo A, cómo invertiría su orden?.
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
